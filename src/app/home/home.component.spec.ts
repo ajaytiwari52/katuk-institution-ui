0921c261bb6c43e3c44946efa9aa725aefe779b5
Ajay Kumar Tiwari <ajay11071982@gmail.com>
2018-03-26 19:16:07 +0530

    added test for home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,25 +1,72 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {TestBed, inject, getTestBed, async} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HomeComponent} from './home.component';
+import {HomeService} from './shared/home.service';
+import {Observable} from 'rxjs/Observable';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRouteStub} from '../../testing/activated-route-stub';
+import {HomeServiceStub} from '../../testing/HomeServiceStub';
 
-import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
-
+  let testBed: TestBed;
+  let homeServiceStub: Partial<HomeService>;
+  let homeService: HomeService;
+  let homeComponent;
+  let fixture;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+  const httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+  homeServiceStub = <HomeService>new HomeServiceStub();
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      providers: [{provide: HomeService, useValue: homeServiceStub}, {provide: ActivatedRoute, useValue: {
+      data: {
+        subscribe: (fn: (value: Data) => void) => fn({
+          company: "",
+        }),
+      },
+      params: {
+        subscribe: (fn: (value: Params) => void) => fn({
+          tab: 0,
+        }),
+      },
+      snapshot: {
+        url: [
+          {
+            path: 'foo',
+          },
+          {
+            path: 'bar',
+          },
+          {
+            path: 'baz',
+          },
+        ],
+        paramMaps: { get: ()=>{ return ""}}
+      },
+    }},
+        {provide: Router, useValue: routerSpy}]
+    });
+    testBed = getTestBed();
+    fixture = testBed.createComponent(HomeComponent);
+    homeComponent = fixture.componentInstance;
+    homeService = testBed.get(HomeService);
+    // fixture.detectChanges();
+  });
+  it('stub object and injected UserService should be the same', () => {
+    expect(homeServiceStub === homeService).toBe(true);
+  });
+  it('should navigate to', () => {
+    const courses = [
+      {
+        'id': 'fs',
+        'name': 'First Semester',
+        'price': 2000,
+        'duration': 3
+      }];
+    homeComponent.ngOnInit();
+    expect(homeServiceStub.getCourses).toHaveBeenCalled();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
