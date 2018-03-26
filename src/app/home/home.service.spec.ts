84658ab429e5a8d8d28d5f0066ea77765cbfb764
Ajay Kumar Tiwari <ajay11071982@gmail.com>
2018-03-26 18:10:00 +0530

    test for home service

--- src/app/home/home.service.spec.ts
+++ src/app/home/home.service.spec.ts
@@ -1,15 +1,43 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject, getTestBed, async} from '@angular/core/testing';
+import {HomeService} from './shared/home.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {CourseModel} from './shared/course.model';
 
-import { HomeService } from './home.service';
 
 describe('HomeService', () => {
+  let service: HomeService;
+  let testBed: TestBed;
+  let httpMock: HttpTestingController;
+  let courses: CourseModel[];
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [HomeService]
     });
+    testBed = getTestBed();
+    httpMock = TestBed.get(HttpTestingController);
+    courses = [
+      {
+        'id': 'fs',
+        'name': 'First Semester',
+        'price': 2000,
+        'duration': 3
+      }
+
   });
 
   it('should be created', inject([HomeService], (service: HomeService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should return courses', async(inject([HomeService], (service: HomeService) => {
+    service.getCourses().subscribe(results => {
+      expect(results).toBeDefined();
+      expect(results.data).toEqual(courses);
+    });
+    const request = httpMock.expectOne('http://localhost:3004/courses');
+    expect(request.request.method).toEqual('GET');
+    request.flush({message:"Success", data:courses});
+    httpMock.verify();
+  })));
 });
