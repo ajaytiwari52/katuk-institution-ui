5a44066d58ef005a650739dc76dbd81f05e5264a
Ajay Kumar Tiwari <ajay11071982@gmail.com>
2018-04-03 16:41:03 +0530

    Add files via upload

--- src/app/home/shared/home.service.ts
+++ src/app/home/shared/home.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {CourseModel} from './course.model';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class HomeService {
 
   constructor(private http: HttpClient) { }
 
-  public getCourses() {
+  public getCourses(): Observable<CourseModel[]> {
     return this.http.get<CourseModel[]>('http://localhost:3004/courses');
   }
 
